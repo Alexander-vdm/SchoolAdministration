9db0bc1585ee9a26a0116f0c8cf04a22ff48f043
Maria-Veronica-Moise <moisemariaveronica@gmail.com>
2025-03-29 10:45:20 +0200

    yy

--- SchoolAdministration/CourseNumberGenerator.cs
+++ SchoolAdministration/CourseNumberGenerator.cs
@@ -10,8 +10,10 @@ namespace SchoolAdministration
     {
         public string GenerateCourseNumber(DateOnly startDate, string courseName, ushort level, string className)
         {
-            int startYear = startDate.Year;
-            int endYear = startYear + 1;
+            //int startYear = startDate.Year;
+            //int endYear = startYear + 1;
+            string startYear = startDate.ToString("yy");
+            string endYear = startDate.AddYears(1).ToString("yy");
             string code = courseName.Substring(0, 3).ToUpper();
 
             string courseNumber = $"{startYear}{endYear}{code}{level}{className}";
